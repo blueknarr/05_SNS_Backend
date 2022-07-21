5e323726b59a48f728d98f6d11581f58a67fc654
Yong Su Jeong <knarrblue@gmail.com>
2022-07-21 12:57:59 +0900

    Feat - 로그인 기능 및 url

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from user.views import UserCreateView
+from user.views import (
+    UserCreateView,
+    UserView
+)
 
 urlpatterns = [
-    path('register/', UserCreateView.as_view()),
+    path('register/', UserCreateView.as_view(), name='register'),
+    path('login/', UserView.as_view(), name='register'),
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,9 +1,26 @@
+from django.contrib.auth import authenticate
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from user.serializers import UserCreateSerializer
+from user.utils.utils import create_jwt_pair_for_user
+
+
+class UserView(APIView):
+    def post(self, request):
+        user = authenticate(email=request.data['email'], password=request.data['password'])
+
+        if user is not None:
+            tokens = create_jwt_pair_for_user(user)
+            return Response({
+                'message': 'Login 성공',
+                'tokens': tokens
+            }, status=status.HTTP_200_OK)
+        return Response({
+            'message': 'email 또는 password를 확인해주세요.'
+        })
 
 
 class UserCreateView(APIView):
