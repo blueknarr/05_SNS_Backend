49de6a44f370b9b1d226e88622c521fdd1bdee7d
Yong Su Jeong <knarrblue@gmail.com>
2022-07-22 22:46:25 +0900

    Refactor - post model

--- apps/post/models.py
+++ apps/post/models.py
@@ -4,9 +4,10 @@ from user.models import User
 
 # Create your models here.
 class Post(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField('제목', max_length=100, blank=True)
-    content = models.TextField('본문')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField('제목', max_length=100, null=False)
+    writer = models.CharField('작성자', max_length=20, null=False)
+    content = models.TextField('본문', null=False)
     like = models.IntegerField('좋아요', default=0)
     view = models.IntegerField('조회수', default=0)
     create_date = models.DateTimeField('만든 날짜', auto_now_add=True)
