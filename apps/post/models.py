0d2dcde7476851cc76bf61b1ab7c3fd81341db08
Yong Su Jeong <knarrblue@gmail.com>
2022-07-26 06:31:31 +0900

    Feat - 해시태그, 좋아요 모델

--- apps/post/models.py
+++ apps/post/models.py
@@ -1,5 +1,3 @@
-import datetime, pytz
-
 from django.db import models
 from user.models import User
 
@@ -41,7 +39,8 @@ class Post(SoftDeleteModel):
     title = models.CharField('제목', max_length=100, null=False)
     writer = models.CharField('작성자', max_length=20, null=False)
     content = models.TextField('본문', null=False)
-    like = models.IntegerField('좋아요', default=0)
+    hashtag = models.ManyToManyField('PostTag', related_name ='HashTags')
+    like_users = models.ManyToManyField('PostLike', related_name ='Like')
     view = models.IntegerField('조회수', default=0)
     create_date = models.DateTimeField('만든 날짜', auto_now_add=True)
     modify_date = models.DateTimeField('수정한 날짜', auto_now=True)
@@ -53,3 +52,13 @@ class Post(SoftDeleteModel):
 
     def __str__(self):
         return self.title
+
+
+class PostTag(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    tag = models.CharField(max_length=20)
+
+
+class PostLike(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user_id = models.IntegerField('user_id')
