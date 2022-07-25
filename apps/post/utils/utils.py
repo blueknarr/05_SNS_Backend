a2509bf22a7eeff0ecdc6b0738775786fb53495a
Yong Su Jeong <knarrblue@gmail.com>
2022-07-25 15:25:44 +0900

    Feat - 게시글 조회 시 조회 수 증가

--- apps/post/utils/utils.py
+++ apps/post/utils/utils.py
@@ -1,6 +1,7 @@
 import jwt
 
 from sns.settings import SIMPLE_JWT
+from post.models import Post
 
 
 def get_user_id_from_token(token):
@@ -10,3 +11,13 @@ def get_user_id_from_token(token):
         algorithms=[SIMPLE_JWT['ALGORITHM']],
     )
     return decode['user_id']
+
+
+def get_post_detail(post_id):
+    post = Post.objects.filter(id=post_id).first()
+
+    if post:
+        post.view += 1
+        post.save()
+        return post
+    return False
