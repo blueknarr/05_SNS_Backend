699e9af80c783f8ad7497adb2915131f171aebfa
Yong Su Jeong <knarrblue@gmail.com>
2022-07-26 22:41:11 +0900

    Refactor - tag, like 테이블 변경

--- apps/post/models.py
+++ apps/post/models.py
@@ -34,13 +34,21 @@ class SoftDeleteModel(models.Model):
         self.save(update_fields=["is_deleted"])
 
 
+class PostTag(models.Model):
+    tag = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = 'tag'
+        db_table = 'tb_tag'
+
+
 class Post(SoftDeleteModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField('제목', max_length=100, null=False)
     writer = models.CharField('작성자', max_length=20, null=False)
     content = models.TextField('본문', null=False)
-    hashtag = models.ManyToManyField('PostTag', related_name ='HashTags')
-    like_users = models.ManyToManyField('PostLike', related_name ='Like')
+    hashtag = models.ManyToManyField(to=PostTag, related_name='HashTags')
+    like = models.ManyToManyField(to=User, related_name='Like')
     view = models.IntegerField('조회수', default=0)
     create_date = models.DateTimeField('만든 날짜', auto_now_add=True)
     modify_date = models.DateTimeField('수정한 날짜', auto_now=True)
@@ -52,13 +60,3 @@ class Post(SoftDeleteModel):
 
     def __str__(self):
         return self.title
-
-
-class PostTag(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    tag = models.CharField(max_length=20)
-
-
-class PostLike(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    user_id = models.IntegerField('user_id')
