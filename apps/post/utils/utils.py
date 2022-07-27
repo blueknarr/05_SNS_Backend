0ef65e903a8e6bc890397fb23cc36bd58c59e597
Yong Su Jeong <knarrblue@gmail.com>
2022-07-27 19:14:49 +0900

    Feat - 좋아요 및 해시태그 get, set

--- apps/post/utils/utils.py
+++ apps/post/utils/utils.py
@@ -1,7 +1,7 @@
 import jwt
 
 from sns.settings import SIMPLE_JWT
-from post.models import Post
+from post.models import Post, PostTag
 
 
 def get_user_id_from_token(token):
@@ -32,9 +32,38 @@ def set_post_like_cnt(sel, user_id, post_id):
         if is_liked:
             msg = f'{post_id}번 게시글의 좋아요를 이미 눌렀습니다.'
         else:
-            msg = f'{post_id}번 게시글의 좋아요가 눌렸습니다.'
+            post.like.add(user_id)
+            msg = f'{post_id}번 게시글의 좋아요를 눌렸습니다.'
     elif sel == 'dislike':
         post.like.remove(user_id)
         msg = f'{post_id}번 게시글의 좋아요를 취소했습니다.'
 
     return msg
+
+
+def get_post_like_cnt(post_id, is_deleted=False):
+    if is_deleted:
+        post = Post.deleted_objects.get(id=post_id)
+    else:
+        post = Post.objects.get(id=post_id)
+    cnt = post.like.all()
+
+    return cnt.count()
+
+
+def set_hash_tag(post_id, tags):
+    post = Post.objects.get(id=post_id)
+
+    for name in tags.split(','):
+        tag, is_created = PostTag.objects.get_or_create(tag=name)
+        post.hashtag.add(tag)
+
+
+def get_hash_tag(post_id, is_deleted=False):
+    if is_deleted:
+        post = Post.deleted_objects.get(id=post_id)
+    else:
+        post = Post.objects.get(id=post_id)
+    tags = post.hashtag.all()
+
+    return [row.tag for row in tags]
