0df0ac2d0f3992a56d6ebc38dcc727cfbb4cbfc4
Yong Su Jeong <knarrblue@gmail.com>
2022-07-27 19:14:13 +0900

    Refactor - post id, user id

--- apps/post/serializers.py
+++ apps/post/serializers.py
@@ -33,10 +33,10 @@ class PostPatchSerializer(serializers.ModelSerializer):
 class PostDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ['id', 'title', 'writer', 'content', 'like', 'view', 'hashtag', 'create_date', 'modify_date', 'is_deleted']
+        fields = ['id', 'user_id', 'title', 'writer', 'content', 'like', 'view', 'hashtag', 'create_date', 'modify_date', 'is_deleted']
 
 
 class PostListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ['title', 'writer', 'create_date', 'like', 'view']
+        fields = ['id', 'user_id', 'title', 'writer', 'create_date', 'like', 'view']
