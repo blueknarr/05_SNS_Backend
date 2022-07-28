841b130f0fa61498c61349bb0c6ac5b6a7899fa4
Yong Su Jeong <knarrblue@gmail.com>
2022-07-28 20:24:52 +0900

    Doc - Docstrings

--- apps/post/utils/utils.py
+++ apps/post/utils/utils.py
@@ -5,6 +5,11 @@ from post.models import Post, PostTag
 
 
 def get_user_id_from_token(token):
+    '''
+    access_token을 decode하여 user_id를 비교하여 본인 여부 확인
+    :param token: String
+    :return: String
+    '''
     decode = jwt.decode(
         token,
         SIMPLE_JWT['SIGNING_KEY'],
@@ -14,6 +19,11 @@ def get_user_id_from_token(token):
 
 
 def get_post_detail(post_id):
+    '''
+    게시글을 열람했을 때 조회수 증가
+    :param post_id: Int
+    :return: Boolean
+    '''
     post = Post.objects.filter(id=post_id).first()
 
     if post:
@@ -24,6 +34,14 @@ def get_post_detail(post_id):
 
 
 def set_post_like_cnt(sel, user_id, post_id):
+    '''
+    게시글에 대한 좋아요 기능
+    하나의 게시글에 좋아요를 중복 선택 불가
+    :param sel: String
+    :param user_id: Int
+    :param post_id: Int
+    :return: String
+    '''
     post = Post.objects.get(id=post_id)
 
     if sel == 'like':
@@ -42,6 +60,12 @@ def set_post_like_cnt(sel, user_id, post_id):
 
 
 def get_post_like_cnt(post_id, is_deleted=False):
+    '''
+    게시글의 좋아요 개수
+    :param post_id: Int
+    :param is_deleted: Boolean
+    :return: Int
+    '''
     if is_deleted:
         post = Post.deleted_objects.get(id=post_id)
     else:
@@ -52,6 +76,11 @@ def get_post_like_cnt(post_id, is_deleted=False):
 
 
 def set_hash_tag(post_id, tags):
+    '''
+    게시글에 대한 해시 태크 생성
+    :param post_id: Int
+    :param tags: List
+    '''
     post = Post.objects.get(id=post_id)
 
     for name in tags.split(','):
@@ -60,6 +89,13 @@ def set_hash_tag(post_id, tags):
 
 
 def get_hash_tag(post_id, is_deleted=False, filter_tags=None):
+    '''
+    게시글 해시테그
+    :param post_id: Int
+    :param is_deleted: Boolean
+    :param filter_tags: List
+    :return: List
+    '''
     if is_deleted:
         post = Post.deleted_objects.get(id=post_id)
     else:
@@ -71,7 +107,6 @@ def get_hash_tag(post_id, is_deleted=False, filter_tags=None):
 
         cnt = 0
         for row in tags:
-            hash_tag = row.tag[1:]
             if row.tag[1:] in filtered:
                 cnt+=1
         if cnt != len(filtered):
